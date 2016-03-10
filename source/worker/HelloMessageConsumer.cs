9012b563ae0b526573fc609bcf4e8ce533d91115
thomas.hodgson <thomas.hodgson@red-gate.com>
2016-03-10 17:28:57 +0000

    re add error examples

--- source/worker/HelloMessageConsumer.cs
+++ source/worker/HelloMessageConsumer.cs
@@ -1,4 +1,6 @@
-﻿using messaging;
+﻿using System;
+using System.Threading;
+using messaging;
 using models.Urls;
 using Serilog;
 
@@ -6,6 +8,8 @@ namespace worker
 {
     class HelloMessageConsumer : MessageConsumer
     {
+        private readonly Random _random = new Random();
+
         public HelloMessageConsumer(IMessageBus messageBus, ILogger logger) :
             base(messageBus, logger, Event.HelloRequested, new[] { RequestUrls.Hello, RequestUrls.Bye })
         {
@@ -14,11 +18,11 @@ namespace worker
         protected override void OnMessage(Message message)
         {
 
-            //            Thread.Sleep(5000);
-            //            if (_random.Next(4) == 0)
-            //            {
-            //                throw new Exception("This is very serious");
-            //            }
+            Thread.Sleep(1000);
+            if (_random.Next(4) == 0)
+            {
+                throw new Exception("This is very serious");
+            }
 
             message.Data.RespondedHelloMessage = $"Hello {message.Data.Name}";
             SendMessage(Event.HelloMessageCreated, message);
